6e331598bc9efc76c9477330df85b2936f659fa5
Kalle Kankare <kalle.kankare@iki.fi>
2013-10-03 21:29:04 +0300

    update comments

--- visfilt.el
+++ visfilt.el
@@ -1,7 +1,6 @@
-;; -*- lexical-binding: t -*-
-;;; visfilt.el --- generic visual filter
+;;; visfilt.el --- generic visual filter -*- lexical-binding: t -*-
 
-;; Copyright (C) 2012 Kalle Kankare
+;; Copyright (C) 2012, 2013 Kalle Kankare
 
 ;; Author: Kalle Kankare <kalle.kankare@iki.fi>
 ;; Maintainer: Kalle Kankare <kalle.kankare@iki.fi>
@@ -11,12 +10,12 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; GNU Emacs is free software: you can redistribute it and/or modify
+;; visfilt is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; visfilt is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
